65d664c79b39e72b9f8f5130aea4276ce78c43c2
Gabriel Oliveira <gabrieloliveira311@outlook.com>
2023-10-10 09:33:12 -0300

    Implementação do banco de dados

--- src/main/java/br/com/gabrieloli/todolist/user/UserController.java
+++ src/main/java/br/com/gabrieloli/todolist/user/UserController.java
@@ -1,5 +1,9 @@
 package br.com.gabrieloli.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,23 +20,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UserController {
     
-    /*
-     * String (Texto)
-     * Integer (int)
-     * Double (decimais 0.0000)
-     * Float (0.000)
-     * char (A C)
-     * Date (data)
-     * void
-     */
-
-     /*
-      * Body
-      */
+    @Autowired
+    private IUserRepository userRepository;
     
     @PostMapping("/")
-    public void create(@RequestBody UserModel userModel) {
-        System.out.println(userModel.name);
+    public ResponseEntity create(@RequestBody UserModel userModel) {
+        var user = this.userRepository.findByUsername(userModel.getUsername());
+
+        if(user != null) {
+            // Mensagem de erro
+            // Status code
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe!");
+        }
+
+        var userCreated = this.userRepository.save(userModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     }
 
 }
